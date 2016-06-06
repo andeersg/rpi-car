bc2bd50bcf47983915aeed10ad051dea5aa82cac
Anders Grendstadbakk <anders.2205@gmail.com>
2016-06-06 23:57:32 +0200

    Handle a simple JSON flow

--- playing.py
+++ playing.py
@@ -11,16 +11,37 @@
 #
 # Instead of the iPhone app use a web interface with sockets or something,
 # that reads from the same db/file and controls mpc
+import json;
+from time import sleep
 
 def main():
+
+    debug_show = 'show1'
+    debug_time = 1
 
     keep_looping = True
     while (keep_looping):
         # Get current status
-        status = run_cmd("mpc status" )
+        # status = run_cmd("mpc status" )
+        # status = "testing"
         # Parse status to get time.
-        print(status)
+        print(debug_show + " - " + str(debug_time))
         # if playing update config.
-
+        registerPlaytime(debug_show, debug_time)
+        debug_time += 1
         # Check every 5 seconds.
         sleep(5)
+
+def registerPlaytime(show, elapsed):
+    json_data = open('config.json', 'r')
+    data = json.load(json_data)
+
+    data[show] = elapsed
+
+    json_data = open('config.json', 'w+')
+    json_data.write(json.dumps(data))
+    json_data.close()
+
+
+
+main()
